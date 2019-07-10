de460e239cbd5b043e94467d90f434d00cd99820
Anatoly <salovanatoly@mail.ru>
2019-07-10 16:03:35 +0300

    Добавили домашнее задание 4

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let money = +prompt("Ваш бюджет на месяц?");
+let money, time;
+//
+function start () {
+money = +prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD"); 
 
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+while(isNaN(money) || money=="" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?");
+}
+}
+start ();
+
+
 
 let appData ={
     budget : money,
@@ -8,45 +18,90 @@ let appData ={
     expenses:{},
     optionalExpenses:{},
     income:[],
-    savings: false,
+    savings: true,
 }
 
-i = 0;
-do {
-    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
-    b = prompt("Во сколько обойдется?",'');    
-    i++
-
+ function chooseExpenses() {
+    for (i=0; i<2; i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = prompt("Во сколько обойдется?",'');  
+    
     if ( (typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && a.length<50 && b != ''){  
-        console.log("if "+i+" = fully implemented");
-        console.log("а (статья расходов)= "+a);
-        console.log("b (сколько стоит)= "+b);
-        appData.expenses[a]=b;  
+    console.log("if "+i+" = fully implemented");
+    console.log("а (статья расходов)= "+a);
+    console.log("b (сколько стоит)= "+b);
+    //Записать ответы в объект expenses в формате: 
+    // expenses: {
+    //     “ответ на первый вопрос” : “ответ на второй вопрос”
+    //     }
+    appData.expenses[a]=b;
+    }
+    else {
+        console.log("if "+i+"Проверка не сработала")
+        i=i-1
+    }
+    };  
+ }
+ chooseExpenses();
+
+ // 1) Оформить расчет дневного бюджета  и вывод на экран этого значения как одну функцию (detectDayBudget)
+
+function detectDayBudget () {
+    appData.moneyPerDay=(appData.budget/30).toFixed();
+    alert("Бюджет на один день "+appData.moneyPerDay);
+}
+detectDayBudget ()
+
+// 2) Оформить блок кода с расчетом уровня достатка как отдельную функцию (detectLevel)
+function detectLevel () {
+    if (appData.moneyPerDay<20) {
+        console.log("Минимальный уровень достатка");
+    }
+    else if (appData.moneyPerDay >20 && appData.moneyPerDay <50){
+        console.log("Средний уровень достатка");
+    }
+    else if (appData.moneyPerDay >50 && appData.moneyPerDay <100){
+        console.log("Высокий уровень достатка");
+    }
+    else if (appData.moneyPerDay >100){
+        console.log("Очень высокий достаток");
+    }
+    else {
+        console.log("Произошла ошибка");
+    }
+    
+}
+detectLevel () 
+
+function checkSaving () {
+    if (appData.savings ==true){
+        let save = +prompt("Какова сумма накоплений");
+            percent = +prompt("Под какой процент");
+
+            appData.monthIncome =save/100/12*percent;
+            alert ("Доход в месяц с вашего депозита " + (appData.monthIncome).toFixed() );
+
+            console.log (appData);
     }
 }
-while(i<2);
 
-appData.moneyPerDay=appData.budget/30;
+checkSaving ()
 
-alert("Бюджет на один день "+appData.moneyPerDay);
+// 3) Создать функцию для определения необязательных расходов (chooseOptExpenses):
+// Необходимо 3 раза спросить у пользователя “Статья необязательных расходов?”
+// Записать ответы в объект optionalExpenses в формате Номер - Ответ.
+//     optionalExpenses: {
+//     1 : “ответ на вопрос”
+//     }
+// Вызывать функции не обязательно.
 
-if (appData.moneyPerDay<20) {
-    console.log("Минимальный уровень достатка");
-}
-else if (appData.moneyPerDay >20 && appData.moneyPerDay <50){
-    console.log("Средний уровень достатка");
-}
-else if (appData.moneyPerDay >50 && appData.moneyPerDay <100){
-    console.log("Высокий уровень достатка");
-}
-else if (appData.moneyPerDay >100){
-    console.log("Очень высокий достаток");
-}
-else {
-    console.log("Произошла ошибка");
+function chooseOptExpenses () {
+    for (i=0; i<3; i++){ // i<3 - 3 раза задать вопросы
+        let opt = prompt("Статья необязательных расходов",''); 
+        appData.optionalExpenses[i]=opt;
+        console.log ("Записалось"+opt);
 }
 
-console.log(appData.expenses);
-console.log("appData.budget= "+appData.budget);
-console.log("appData.timeData= "+appData.timeData);
-console.log("appData.moneyPerDay= "+appData.moneyPerDay);
+};  
+
+chooseOptExpenses ()
